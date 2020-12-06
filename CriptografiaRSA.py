027d6c46fd3f471244138de2c1c555eb91208539
EliasNsilva <57777232+EliasNsilva@users.noreply.github.com>
2020-12-06 12:18:15 -0300

    Update CriptografiaRSA.py

--- CriptografiaRSA.py
+++ CriptografiaRSA.py
@@ -1,56 +1,36 @@
 import math 
+import time
+
+def progress_bar(done):
+    print("\rProcessando: [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100),end='')
 
 
-def desencriptar(inverso, chave1):
-    arquivo = open('Mensagem_Encriptada.txt', 'r')
-    mensagem_encrip = arquivo.read()
-    des_encrip = open('Mensagem_Desencriptada.txt', 'w')
-    arquivo.close()
+def test():
+    for n in range(4):
+        progress_bar(n/3)
+        time.sleep(1)
+    print('\n')
 
+def e_primo(x):
+    i = 2
 
-    i = 0
+    while(1):
+        if i > math.sqrt(x):
+            return 1  
+        if x % i == 0:
+            return 0   
+        i = i + 1 
 
-    while(i < len(mensagem_encrip)):
-        n = ''
-        while(1):
-            if mensagem_e[i] == ' ':
-                letra = pow(int(n), inverso)%chave1
-                if letra == 28:
-                    des_encrip.write(' ')
-                else:
-                    des_encrip.write(chr(letra + 63))
-                    break
-            n = n + str(mensagem_encrip)
-            i = i + 1
-        i = i +1
+def mdc(x,y):
+    while(1):
+        if y == 1:
+            return 1
+        if y == 0:
+            return 0
 
-    mensagem_encrip.close()
-    des_encrip.close()
-
-
-
-def encriptar(mensagem, chave_publi1, chave_publi2):
-    i = 0
-
-    arquivo = open('Mensagem_Encriptada.txt', 'w')
-    
-    while(i < len(mensagem)):
-
-        if ord(mensagem[i]) == 32:
-            cript = 28
-        elif ord(mensagem[i]) > 64 and ord(mensagem[i]) < 97:
-            cript = ord(mensagem[i]) - 63
-        elif ord(mensagem[i]) > 96:
-            cript = ord(mensagem[i]) - 95
-
-        cript = pow(cript,chave_publi2)%chave_publi1    
-        
-        arquivo.write(str(cript))
-        arquivo.write(' ')
-
-        i = i + 1
-
-    arquivo.close()
+        aux = y
+        y = x % y
+        x = aux 
 
 
 def calcular_inverso(e,totiente_de_euler):
@@ -85,28 +65,62 @@ def calcular_inverso(e,totiente_de_euler):
     if verificar_negatividade % 2 == 0:
         n_anterior = n_anterior * (-1)
 
+    print('Inverso:', n_anterior)
     return n_anterior
 
-def e_primo(x):
-    i = 2
 
-    while(1):
-        if i > math.sqrt(x):
-            return 1  
-        if x % i == 0:
-            return 0   
-        i = i + 1 
 
-def mdc(x,y):
-    while(1):
-        if y == 1:
-            return 1
-        if y == 0:
-            return 0
+def desencriptar(inverso, chave1):
+    arquivo = open('Mensagem_Encriptada.txt', 'r')
+    mensagem_encrip = arquivo.read()
+    des_encrip = open('Mensagem_Desencriptada.txt', 'w')
+    arquivo.close()
 
-        aux = y
-        y = x % y
-        x = aux 
+
+    i = 0
+
+    while(i < len(mensagem_encrip)):
+        n = ''
+        while(1):
+            if mensagem_encrip[i] == " ":
+                letra = pow(int(n), inverso)%chave1
+                if letra == 28:
+                    des_encrip.write(' ')
+                else:
+                    des_encrip.write(chr(letra + 63))
+                    break
+            n = n + str(mensagem_encrip[i])
+            i = i + 1
+        i = i +1
+
+    arquivo.close()
+    des_encrip.close()
+
+
+
+def encriptar(mensagem, chave_publi1, chave_publi2):
+    i = 0
+
+    open('Mensagem_Encriptada.txt', 'w')
+    arquivo = open('Mensagem_Encriptada.txt', 'a')
+    
+    while(i < len(mensagem)):
+
+        if ord(mensagem[i]) == 32:
+            cript = 28
+        elif ord(mensagem[i]) > 64 and ord(mensagem[i]) < 97:
+            cript = ord(mensagem[i]) - 63
+        elif ord(mensagem[i]) > 96:
+            cript = ord(mensagem[i]) - 95
+
+        cript = pow(cript,chave_publi2)%chave_publi1    
+        
+        arquivo.write(str(cript))
+        arquivo.write(' ')
+
+        i = i + 1
+
+    arquivo.close()
 
 
 while(1):
@@ -114,6 +128,7 @@ while(1):
     print('1 - Gerar Chave Pública\n')
     print('2 - Encriptar\n')
     print('3 - Desencriptar\n')
+    print('0 - Sair do programa\n')
 
     opcao = int(input())
 
@@ -144,7 +159,11 @@ while(1):
         arquivo.write(str(chave1))
         arquivo.write('\n')
         arquivo.write(str(chave2))
-        arquivo.close
+        arquivo.close()
+        print('\n')
+        test()
+        print('Chaves geradas com sucesso!\n')
+        print('_____________________________________\n')
 
     
     if opcao == 2:
@@ -156,6 +175,11 @@ while(1):
         chave_publi2 = int(input())
 
         encriptar(mensagem, chave_publi1, chave_publi2)
+        print('\n')
+        test()
+        print('Mensagem Encriptada com sucesso!\n')
+        print('_____________________________________\n')
+
 
 
     if opcao == 3:
@@ -170,8 +194,11 @@ while(1):
         chave1 = p*q
         inverso = calcular_inverso(e,tontiente)
 
-        desencriptar(inverso, chave1)  
-        print('Mensagem Desencriptada com sucesso!')  
+        desencriptar(inverso, chave1)
+        print('\n')
+        test() 
+        print('Mensagem Desencriptada com sucesso!\n')
+        print('_____________________________________\n')
 
-
-    break
+    if opcao == 0:
+        break
